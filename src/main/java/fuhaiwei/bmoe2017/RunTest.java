a6af9e9be2f9a22b8809f5e90284171ad4a96f3c
fuhaiwei <fuhaiwei@126.com>
2017-08-11 10:16:13 +0800

    fix test path

--- src/main/java/fuhaiwei/bmoe2017/RunTest.java
+++ src/main/java/fuhaiwei/bmoe2017/RunTest.java
@@ -13,8 +13,8 @@ public class RunTest {
     public static void main(String[] args) {
         try {
             System.out.println(handleData(
-                    new JSONObject(readText("/Users/fuhaiwei/workspace/bmoe-2017/bmoe-json/2017-08-09/current.txt")),
-                    new JSONArray(readText("/Users/fuhaiwei/workspace/bmoe-2017/bmoe-json/2017-08-09/2017-08-09 23:10:09.txt"))));
+                    new JSONObject(readText("bmoe-json/2017-08-10/current.txt")),
+                    new JSONArray(readText("bmoe-json/2017-08-10/2017-08-10 23:10:09.txt"))));
         } catch (IOException e) {
             e.printStackTrace();
         }
